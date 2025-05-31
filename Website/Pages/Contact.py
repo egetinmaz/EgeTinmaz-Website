44d3b705e5b3d88399b19c8753e2dc7d251b5e45
Kaitlin Kirkes <egetinmaz@gmail.com>
2025-05-30 20:31:54 -0700

    Added Contact details

--- Website/Pages/Contact.py
+++ Website/Pages/Contact.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Contact Me:", divider="grey")
+
+st.markdown("""
+- **Email**: egetinmaz@gmail.com
+- **Phone**: +1 (650) 200-8293
+- **LinkedIn**: www.linkedin.com/in/ege-tinmaz
+- **GitHub**: https://github.com/egetinmaz
+""")
