e275c8a4cfe0a0d490e304ef281721436a80a18d
Ege Tinmaz <egetinmaz@gmail.com>
2025-05-30 22:40:59 -0700

    Update 1_Contact.py

--- pages/1_Contact.py
+++ pages/1_Contact.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title("Contact")
-
 # Set page configuration
 st.set_page_config(page_title="Contact",initial_sidebar_state="collapsed") 
 margin_r,body,margin_l = st.columns([1, 2, 1])
 
+#Header
+st.title("Contact")
 st.header("Contact Me:", divider="grey")
 
+# Contact Details
 st.markdown("""
 - **Email**: egetinmaz@gmail.com
 - **Phone**: +1 (650) 200-8293
